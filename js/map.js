f5f48ee9d5903499efd7ff29d2f6979e59bb5b24
saveusy2jeff <saveusy2jeff@gmail.com>
2018-04-30 00:00:09 +0400

    Добавил функцию на соответствие значений formRooms и formCapacity

--- js/map.js
+++ js/map.js
@@ -199,7 +199,7 @@ var closePopup = function (evt) {
   exitAd.setAttribute('hidden', true);
 };
 var notHiddenCard;
-var activatePinListener = function (activateAddress, activateAd, buttonExit) {
+var onActivateAddressClick = function (activateAddress, activateAd, buttonExit) {
   activateAddress.addEventListener('click', function () {
     notHiddenCard = document.querySelector('.map__card:not([hidden])');
     if (notHiddenCard !== null) {
@@ -221,5 +221,56 @@ for (var buttonNumber = 0; buttonNumber < buttons.length; buttonNumber++) {
   var activateAddress = buttons[buttonNumber];
   var activateAd = popupRequest[buttonNumber];
   var buttonExit = popupClose[buttonNumber];
-  activatePinListener(activateAddress, activateAd, buttonExit);
+  onActivateAddressClick(activateAddress, activateAd, buttonExit);
 }
+
+ //
+
+var formType = document.querySelector('#type');
+var formMinPrice = {
+  'bungalo': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000
+};
+var formPrice = document.querySelector('#price');
+var formTimeIn = document.querySelector('#timein');
+var formTimeOut = document.querySelector('#timeout');
+var formRooms = document.querySelector('#room_number');
+var formCapacity = document.querySelector('#capacity');
+var rulesForGuests = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0']
+};
+
+var onFormTimeInTimeOutChange = function (time1, time2) {
+  time1.addEventListener('change', function (evt) {
+    time2.value = evt.target.value;
+      })
+  time2.addEventListener('change', function(evt) {
+    time1.value = evt.target.value;
+  })
+}
+var setMinPrice = function () {
+  var price = document.querySelector('#price');
+  price.min = formMinPrice[formType.value];
+  price.placeholder = formMinPrice[formType.value];
+  return price
+};
+var syncNumberRoomsandGuests = function () {
+  var roomNumber = formRooms.value
+  var currentCapacity = formCapacity.value
+  var checkRulesCapacity = rulesForGuests[roomNumber];
+  if (checkRulesCapacity.includes(currentCapacity)) {
+    formCapacity.setCustomValidity('')
+  }
+  else {
+    formCapacity.setCustomValidity('Выбранное значение количества гостей не подходит под количество комнат!')
+  }
+}
+onFormTimeInTimeOutChange(formTimeIn, formTimeOut);
+formType.addEventListener('change', setMinPrice);
+formRooms.addEventListener('change', syncNumberRoomsandGuests);
+formCapacity.addEventListener('change', syncNumberRoomsandGuests);
