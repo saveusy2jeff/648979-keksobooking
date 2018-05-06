f65e6c9c5fed9171e58245e5cbe36ff311f8793b
saveusy2jeff <saveusy2jeff@gmail.com>
2018-05-06 15:36:17 +0400

    Исправляю ошибки (синтаксис)

--- js/backend.js
+++ js/backend.js
@@ -3,7 +3,7 @@
 (function () {
   window.load = function (onLoad, onError) {
     var URL = 'https://js.dump.academy/keksobooking/data';
-    var xhr = new XMLHTTPRequest();
+    var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
@@ -16,15 +16,15 @@
       } else if (xhr.status === 400) {
         onError('Ошибка! Статус ответа: Неверный запрос ' + xhr.status + ' ' + xhr.statusText);
       } else if (xhr.status === 403) {
-                onError('Ошибка! Статус ответа: Нет прав выполнить запрос ' + xhr.status + ' ' + xhr.statusText);
+        onError('Ошибка! Статус ответа: Нет прав выполнить запрос ' + xhr.status + ' ' + xhr.statusText);
       } else {
         onError('Ошибка! Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
-    xhr.addEventListener ('error', function() {
+    xhr.addEventListener('error', function () {
       onError('произошла ошибка соединения');
     });
-    xhr.addEventListener ('timeout', function () {
+    xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
     xhr.timeout = 10000; // 10 sec
@@ -36,7 +36,7 @@
     var URL = 'https://js.dump.academy/keksobooking';
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    xhr.addEventListener ('load', function () {
+    xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onLoad(xhr.response);
       } else if (xhr.status === 401) {
